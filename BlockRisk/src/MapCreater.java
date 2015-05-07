47b07dcf8c8f1bb5d0c826efe32873e8910ffd67
Abdallah Hassan <lolabbetabbe@gmail.com>
2015-05-07 19:46:30 +0200

    Update MapCreater.java

--- BlockRisk/src/MapCreater.java
+++ BlockRisk/src/MapCreater.java
@@ -1,8 +1,9 @@
+
+import org.lwjgl.input.Keyboard;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.geom.Polygon;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
@@ -11,39 +12,36 @@ public class MapCreater extends BasicGameState {
 	
 	private MouseInput mouseinput;
 	private Map map;
-	private Polygon[] regions;
+	private Territory[] regions;
+	private boolean inputMode;
+	private IntPair coord;
 
 	@Override
 	public void init(GameContainer gc, StateBasedGame sbg)
 			throws SlickException {
 		mouseinput = new MouseInput();
 		map = new Map();
-		regions = new Polygon[12];
+		regions = new Territory[12];
 		
-		float[] cords1 = {0, 50, 200, 50, 200, 150, 0, 150};
-		regions[0] = new Polygon(cords1);
-		float[] cords2 = {0, 150, 200, 150, 200, 300, 0, 300};
-		regions[1] = new Polygon(cords2);
-		float[] cords3 = {0, 300, 200, 300, 200, 450, 0, 450};
-		regions[2] = new Polygon(cords3);
-		float[] cords4 = {200, 50, 350, 50, 350, 200, 200, 200};
-		regions[3] = new Polygon(cords4);
-		float[] cords5 = {200, 200, 350, 200, 350, 450, 200, 450};
-		regions[4] = new Polygon(cords5);
-		float[] cords6 = {350, 50, 550, 50, 550, 150, 350, 150};
-		regions[5] = new Polygon(cords6);
-		float[] cords7 = {350, 150, 550, 150, 550, 250, 350, 250};
-		regions[6] = new Polygon(cords7);
-		float[] cords8 = {350, 250, 550, 250, 550, 400, 350, 400};
-		regions[7] = new Polygon(cords8);
-		float[] cords9 = {350, 400, 800, 400, 800, 450, 350, 450};
-		regions[8] = new Polygon(cords9);
-		float[] cords10 = {550, 50, 700, 50, 700, 300, 550, 300};
-		regions[9] = new Polygon(cords10);
-		float[] cords11 = {550, 300, 800, 300, 800, 400, 550, 400};
-		regions[10] = new Polygon(cords11);
-		float[] cords12 = {700, 50, 800, 50, 800, 300, 700, 300};
-		regions[11] = new Polygon(cords12);
+		makeSquare(0, new IntPair(0, 0), new IntPair(0, 0));
+		makeSquare(1, new IntPair(0, 0), new IntPair(0, 0));
+		makeSquare(2, new IntPair(0, 0), new IntPair(0, 0));
+		makeSquare(3, new IntPair(0, 0), new IntPair(0, 0));
+		makeSquare(4, new IntPair(0, 0), new IntPair(0, 0));
+		makeSquare(5, new IntPair(0, 0), new IntPair(0, 0));
+		makeSquare(6, new IntPair(0, 0), new IntPair(0, 0));
+		makeSquare(7, new IntPair(0, 0), new IntPair(0, 0));
+		makeSquare(8, new IntPair(0, 0), new IntPair(0, 0));
+		makeSquare(9, new IntPair(0, 0), new IntPair(0, 0));
+		makeSquare(10, new IntPair(0, 0), new IntPair(0, 0));
+		makeSquare(11, new IntPair(0, 0), new IntPair(0, 0));
+		
+		inputMode = false;
+		coord = new IntPair(0, 0);
+	}
+	
+	private void makeSquare(int id, IntPair from, IntPair to) {
+		
 	}
 
 	@Override
@@ -51,31 +49,39 @@ public class MapCreater extends BasicGameState {
 			throws SlickException {
 		g.setColor(Color.white);
 		g.fillRect(Main.UPPER_LEFT_CORNER.x, Main.UPPER_LEFT_CORNER.y, Main.LOWER_RIGHT_CORNER.x - Main.UPPER_LEFT_CORNER.x, Main.LOWER_RIGHT_CORNER.y - Main.UPPER_LEFT_CORNER.y);
+		Color tmp;
 		for (int i = 0; i < 12; i++) {
 			if (i == 0 || i == 6)
-				g.setColor(Color.blue);
+				tmp = Color.blue;
 			else if (i == 1 || i == 7)
-				g.setColor(Color.cyan);
+				tmp = Color.cyan;
 			else if (i == 2 || i == 8)
-				g.setColor(Color.green);
+				tmp = Color.green;
 			else if (i == 3 || i == 9)
-				g.setColor(Color.red);
+				tmp = Color.red;
 			else if (i == 4 || i == 10)
-				g.setColor(Color.yellow);
+				tmp = Color.yellow;
 			else if (i == 5 || i == 11)
-				g.setColor(Color.orange);
+				tmp = Color.orange;
 			else
-				g.setColor(Color.pink);
+				tmp = Color.pink;
 			
-			g.fill(regions[i]);
+			regions[i].draw(g, tmp);
 		}
 	}
 
 	@Override
 	public void update(GameContainer gc, StateBasedGame sbg, int delta)
 			throws SlickException {
-		// TODO Auto-generated method stub
+		mouseinput.update();
 		
+			if (Keyboard.getEventKey() == Keyboard.KEY_F) {
+				inputMode = false;
+			} else if (Keyboard.getEventKey() == Keyboard.KEY_T) {
+				inputMode = true;
+			}
+			
+						
 	}
 
 	@Override
