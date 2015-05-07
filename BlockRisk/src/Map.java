d133d1a46f74264ab2f0ed4151160a40b9085eaf
Abdallah Hassan <lolabbetabbe@gmail.com>
2015-05-07 14:31:32 +0200

    Update Map.java

--- BlockRisk/src/Map.java
+++ BlockRisk/src/Map.java
@@ -1,10 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Map {
 	
 	private Territory[] territories;
+	private List<IntPair> neighboors;
 	
 	public Map() {
 		territories = new Territory[12]; // 12 territories
+		neighboors = new ArrayList<IntPair>();
 	}
 	
 	// Load and already saved game from file.
@@ -23,6 +28,10 @@ public class Map {
 	
 	public Territory[] getAllTerritories() {
 		return territories;
+	}
+	
+	public boolean adjacent(int id1, int id2) {
+		return neighboors.contains(new IntPair(Math.min(id1, id2), Math.max(id1, id2)));
 	}
 
 }
