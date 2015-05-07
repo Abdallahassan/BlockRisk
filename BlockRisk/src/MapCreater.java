ac5ff57c884eb51cffae1b16ce077cdaf0639506
Abdallah Hassan <lolabbetabbe@gmail.com>
2015-05-07 20:07:34 +0200

    Update MapCreater.java

--- BlockRisk/src/MapCreater.java
+++ BlockRisk/src/MapCreater.java
@@ -23,25 +23,25 @@ public class MapCreater extends BasicGameState {
 		map = new Map();
 		regions = new Territory[12];
 		
-		makeSquare(0, new IntPair(0, 0), new IntPair(0, 0));
-		makeSquare(1, new IntPair(0, 0), new IntPair(0, 0));
-		makeSquare(2, new IntPair(0, 0), new IntPair(0, 0));
-		makeSquare(3, new IntPair(0, 0), new IntPair(0, 0));
-		makeSquare(4, new IntPair(0, 0), new IntPair(0, 0));
-		makeSquare(5, new IntPair(0, 0), new IntPair(0, 0));
-		makeSquare(6, new IntPair(0, 0), new IntPair(0, 0));
-		makeSquare(7, new IntPair(0, 0), new IntPair(0, 0));
-		makeSquare(8, new IntPair(0, 0), new IntPair(0, 0));
-		makeSquare(9, new IntPair(0, 0), new IntPair(0, 0));
-		makeSquare(10, new IntPair(0, 0), new IntPair(0, 0));
-		makeSquare(11, new IntPair(0, 0), new IntPair(0, 0));
+		makeSquare(0, new IntPair(0, 50), new IntPair(190, 140));
+		makeSquare(1, new IntPair(0, 150), new IntPair(190, 290));
+		makeSquare(2, new IntPair(0, 300), new IntPair(190, 440));
+		makeSquare(3, new IntPair(200, 50), new IntPair(340, 190));
+		makeSquare(4, new IntPair(200, 200), new IntPair(340, 440));
+		makeSquare(5, new IntPair(350, 50), new IntPair(540, 140));
+		makeSquare(6, new IntPair(350, 150), new IntPair(540, 240));
+		makeSquare(7, new IntPair(350, 250), new IntPair(540, 390));
+		makeSquare(8, new IntPair(350, 400), new IntPair(790, 440));
+		makeSquare(9, new IntPair(550, 50), new IntPair(690, 290));
+		makeSquare(10, new IntPair(550, 300), new IntPair(790, 390));
+		makeSquare(11, new IntPair(700, 50), new IntPair(790, 290));
 		
 		inputMode = false;
 		coord = new IntPair(0, 0);
 	}
 	
 	private void makeSquare(int id, IntPair from, IntPair to) {
-		
+		//TODO
 	}
 
 	@Override
