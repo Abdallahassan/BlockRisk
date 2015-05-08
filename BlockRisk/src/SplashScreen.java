834e6dde8b3e931eff99b2694d5f7528e9b1e944
Abdallah Hassan <lolabbetabbe@gmail.com>
2015-05-08 14:41:57 +0200

    Update SplashScreen.java

--- BlockRisk/src/SplashScreen.java
+++ BlockRisk/src/SplashScreen.java
@@ -1,27 +1,61 @@
+import static org.lwjgl.opengl.GL11.GL_QUADS;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
+import static org.lwjgl.opengl.GL11.glBegin;
+import static org.lwjgl.opengl.GL11.glEnable;
+import static org.lwjgl.opengl.GL11.glEnd;
+import static org.lwjgl.opengl.GL11.glTexCoord2f;
+import static org.lwjgl.opengl.GL11.glVertex2i;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import org.newdawn.slick.*;
 import org.newdawn.slick.geom.Polygon;
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 
 public class SplashScreen extends BasicGameState {
 	
-	Polygon poly;
+	private Texture texture;
 	private int time;
 
 	@Override
 	public void init(GameContainer gc, StateBasedGame sbg)
 			throws SlickException {
 		float[] cords = {0, 0, 50, 0, 100, 50, 75, 75, 50, 25};
-		poly = new Polygon(cords);
-		
+		try {
+			this.texture = TextureLoader.getTexture("JPG", new FileInputStream(new File("res/something.jpg")));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		glEnable(GL_TEXTURE_2D);
 	}
 
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
 			throws SlickException {
-		g.setColor(Color.blue);
-		g.draw(poly);
+		texture.bind();
+		drawTexture();
+	}
+	
+	private void drawTexture() {
+		glBegin(GL_QUADS);  // Draw texture on specified coordinates.
+		glTexCoord2f(0, 0);
+		glVertex2i(0, 0);
+		glTexCoord2f(1, 0);
+		glVertex2i(Main.WIDTH, 0);
+		glTexCoord2f(1, 1);
+		glVertex2i(Main.WIDTH, Main.HEIGHT);
+		glTexCoord2f(0, 1);
+		glVertex2i(0, Main.HEIGHT);
+		glEnd();
 	}
 
 	@Override
