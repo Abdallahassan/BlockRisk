4070815714e2570efc658f7b4a3ae1cf1dcdee26
ArsalanS96 <arsalansyed96@hotmail.com>
2015-05-08 14:18:10 +0200

    Update Territory.java

--- BlockRisk/src/Territory.java
+++ BlockRisk/src/Territory.java
@@ -3,9 +3,9 @@ public class Territory {
 	
 	private boolean highlighted;
 	private boolean owner; // true if it's owned by the AI.
-	private int units; //amount of units in this territory
-	private int unitVal; //amount of units this territory generates each turn 
-	
+	private int[] units; //amount of units in this territory
+	private int resourceVal; //amount of resources this territory generates each turn 
+	private Territory[] neighbours; 
 	
 	public Territory(boolean owner) {
 		this.owner = owner;
@@ -19,16 +19,77 @@ public class Territory {
 		owner = !owner;
 	}
 	
-	public int getUnits(){
+	public int[] getUnits(){
 		return units;
 	}
 	
-	public void setUnits(int remove){
-		units-=remove;
+	
+	public int getResourceVal() {
+		return resourceVal;
+	}
+	
+	/**
+	 * Removes certain type of units from territory. 
+	 * index 0,1,2 = infantry,vehicles,aircraft
+	 * 
+	 * @param index
+	 * @param remove
+	 */
+	public void removeUnits(int index,int remove){
+		units[index]-=remove;
 	}
 
-	public int getUnitVal() {
-		return unitVal;
+	
+	public int sumAttack(){
+		int sum=0;
+		for(int i=0;i<units.length;i++){
+			int numUnit=units[i];
+			int prod=numUnit*attStats[i];
+			sum+=prod;
+		}
+		return sum;
+	}
+	
+	public int sumDefence(){
+		int sum=0;
+		for(int i=0;i<units.length;i++){
+			int numUnit=units[i];
+			int prod=numUnit*defStats[i];
+			sum+=prod;
+		}
+		return sum;
+	}
+	
+	public int averageEvasion(){
+		int sumEv=0;
+		int sumUnits=0;
+		for(int i=0;i<units.length;i++){
+			int numUnit=units[i];
+			sumUnits+=numUnit;
+			sumEv+=numUnit*evStat[i];
+		}		
+		return sumEv/sumUnits;
+	}
+	
+	public boolean isNeighbour(Territory to){
+		for(int i=0;i<neighbours.length;i++){
+			if(neighbours[i].equals(to)){
+				return true;
+			}			
+		}		
+		return false;
+	}
+	
+	public int numUnits(){
+		int sum=0;
+		for(int i=0;i<units.length;i++){
+			sum+=units[i];
+		}
+		return sum;
+	}
+	
+	public Territory[] getNeighbours(){
+		return neighbours;
 	}
 
 }
