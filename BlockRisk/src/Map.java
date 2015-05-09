e82fba953f8589e2412039bffac3ddee39c898bc
Abdallah Hassan <lolabbetabbe@gmail.com>
2015-05-09 11:58:40 +0200

    Update Map.java

--- BlockRisk/src/Map.java
+++ BlockRisk/src/Map.java
@@ -3,14 +3,6 @@ import java.util.List;
 
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
-//need to import jDOM here for map load and save
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.JDOMException;
-import org.jdom.input.SAXBuilder;
 
 
 public class Map {
@@ -22,18 +14,18 @@ public class Map {
 	public Map() {
 		territories = new Territory[12]; // 12 territories
 		neighboors = new ArrayList<IntPair>();
-		squares = new Square[80][40];
-		for (int i = 0; i < 80; i++)
-			for (int j = 0; j < 40; j++)
-				squares[i][j] = new Square(new IntPair(i*10, (j*10)+50));
+		squares = new Square[160][80];
+		for (int i = 0; i < 160; i++)
+			for (int j = 0; j < 80; j++)
+				squares[i][j] = new Square(new IntPair(i*5, (j*5)+50));
 	}
 	
 	public void changeOwner(IntPair coord, int id) {
-		squares[coord.x/10][coord.y/10].setOwnership(id);
+		squares[coord.x/5][(coord.y-50)/5].setOwnership(id);
 	}
 	
 	public void setBound(IntPair coord, boolean val) {
-		squares[coord.x/10][coord.y/10].setBoundary(val);
+		squares[coord.x/5][(coord.y-50)/5].setBoundary(val);
 	}
 	
 	public void draw(Graphics g) {
@@ -55,42 +47,17 @@ public class Map {
 						tmp = Color.orange;
 					else
 						tmp = Color.pink;
+					
+				if (s.boundaryStatus())
+					tmp = Color.black;
 				s.draw(tmp, g);
 			}
 	}
 	
 	// Load and already saved game from file.
 	public void load() {
- 
-	  SAXBuilder builder = new SAXBuilder();
-	  File xmlFile = new File("res/mapSave.xml");
- 
-	  try {
- 
-		Document document = (Document) builder.build(xmlFile); //builds xml file
-		Element rootNode = document.getRootElement(); //gets  a root
-		List list = rootNode.getChildren("staff"); //gets a list of all children of root
- 
-		//iterate over all children to root
-		for (int i = 0; i < list.size(); i++) {
- 
-		   Element node = (Element) list.get(i);
- 
-		   System.out.println("First Name : " + node.getChildText("firstname"));
-		   System.out.println("Last Name : " + node.getChildText("lastname"));
-		   System.out.println("Nick Name : " + node.getChildText("nickname"));
-		   System.out.println("Salary : " + node.getChildText("salary"));
- 
-		}
-
- 		//erro handling
-	  } catch (IOException io) { 
-		System.out.println(io.getMessage());
-	  } catch (JDOMException jdomex) {
-		System.out.println(jdomex.getMessage());
-	  }
+		//TODO
 	}
-
 	
 	// Save this map to save file.
 	public void save() {
