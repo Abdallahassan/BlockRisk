8bb276aa6b06461c6a8d9d4580bba6386bd2da33
Abdallah Hassan <lolabbetabbe@gmail.com>
2015-05-10 11:54:43 +0200

    Update SplashScreen.java

--- BlockRisk/src/SplashScreen.java
+++ BlockRisk/src/SplashScreen.java
@@ -27,7 +27,6 @@ public class SplashScreen extends BasicGameState {
 	@Override
 	public void init(GameContainer gc, StateBasedGame sbg)
 			throws SlickException {
-		float[] cords = {0, 0, 50, 0, 100, 50, 75, 75, 50, 25};
 		try {
 			this.texture = TextureLoader.getTexture("JPG", new FileInputStream(new File("res/SplashScreenFit.jpg")));
 		} catch (FileNotFoundException e) {
