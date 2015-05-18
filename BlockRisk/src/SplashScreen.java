cd77abfabc2f38c8b1308526c8568abefa6adc9a
Abdallah Hassan <lolabbetabbe@gmail.com>
2015-05-18 21:53:29 +0200

    Update SplashScreen.java

--- BlockRisk/src/SplashScreen.java
+++ BlockRisk/src/SplashScreen.java
@@ -17,6 +17,7 @@ import org.newdawn.slick.opengl.Texture;
 import org.newdawn.slick.opengl.TextureLoader;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.util.ResourceLoader;
 
 
 public class SplashScreen extends BasicGameState {
@@ -28,7 +29,7 @@ public class SplashScreen extends BasicGameState {
 	public void init(GameContainer gc, StateBasedGame sbg)
 			throws SlickException {
 		try {
-			this.texture = TextureLoader.getTexture("JPG", new FileInputStream(new File("res/SplashScreenFit.jpg")));
+			this.texture = TextureLoader.getTexture("JPG", ResourceLoader.getResourceAsStream("res/SplashScreenFit.jpg"));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
